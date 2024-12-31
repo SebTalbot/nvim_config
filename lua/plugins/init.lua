f1cb9e045e69d32b99961b3fd33fd87db4d0f27f
Sebastien Talbot <moi@sebastientalbot.com>
2024-12-30 20:35:14 -0500

    theme visual/search colors

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,9 +17,13 @@ local plugins = {
 			require("everforest").setup({})
 			vim.cmd([[colorscheme everforest]])
 			vim.cmd([[hi ColorColumn  guibg=none]])
-			vim.cmd([[hi @character guifg=#d699b6]])
-			vim.cmd([[hi @string.escape guifg=#e69875]])
-			vim.cmd([[hi BufferLineIndicatorSelected guifg=#475258 guibg=#475258]]) -- bugged in bufferline
+			vim.cmd([[hi Visual  guibg=#384B55]])
+			vim.cmd([[hi Search gui=bold guibg=#a7c080]])
+			vim.cmd([[hi IncSearch gui=bold guibg=#7fbbb3]])
+			vim.cmd([[hi @character guifg=#D699B6]])
+			vim.cmd([[hi @string.escape guifg=#E69875]])
+			-- bugged in bufferline
+			vim.cmd([[hi BufferLineIndicatorSelected guifg=#475258 guibg=#475258]])
 
 			vim.cmd([[hi DiagnosticUnderlineError guifg=none]])
 		end,
