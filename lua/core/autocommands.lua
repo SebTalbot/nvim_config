2b2c3d05bb02f7b1adc7b97d8342932c71059a35
Sebastien Talbot <moi@sebastientalbot.com>
2025-01-09 12:34:29 -0500

    autocmd filetype text to conf

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -3,4 +3,5 @@ local cmd = vim.cmd
 cmd([[
 	autocmd BufEnter * if &filetype == "" | setlocal ft=config | endif
 	autocmd BufEnter * if &filetype == "conf" | setlocal ft=config | endif
+	autocmd BufEnter * if &filetype == "text" | setlocal ft=config | endif
 ]])
