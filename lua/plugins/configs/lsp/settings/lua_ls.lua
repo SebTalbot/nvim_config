779f600e15bb235e582b11d1a3e58eb00776bcd8
Sebastien Talbot <moi@sebastientalbot.com>
2025-02-15 15:16:39 -0500

    feat(lua_ls): vimruntime and plugins

--- lua/plugins/configs/lsp/settings/lua_ls.lua
+++ lua/plugins/configs/lsp/settings/lua_ls.lua
@@ -1,17 +1,4 @@
 return {
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
-				},
-			},
-		},
-	},
 	on_attach = function(client, bufnr)
 		client.server_capabilities.document_formatting = false
 		client.server_capabilities.document_range_formatting = false
