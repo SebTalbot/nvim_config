f8964b9f2c1750839c8d224a16d5a110d85af14c
Sebastien Talbot <moi@sebastientalbot.com>
2025-03-31 21:08:25 -0400

    feat(cmp): remove imported snippets

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -373,8 +373,8 @@ local plugins = {
 		"L3MON4D3/LuaSnip",
 		build = "make install_jsregexp",
 		dependencies = {
-			"rafamadriz/friendly-snippets",
-			"saadparwaiz1/cmp_luasnip",
+			-- "rafamadriz/friendly-snippets",
+			-- "saadparwaiz1/cmp_luasnip",
 		},
 	},
 }
