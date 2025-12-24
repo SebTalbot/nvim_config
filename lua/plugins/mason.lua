edad54c8c6a9635264a2ea5b823e644c4b3b7d25
Sebastien Talbot <moi@sebastientalbot.com>
2025-12-24 11:22:15 -0500

    feat(mason): better ensure installed

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,25 @@
 return {
 	"mason-org/mason.nvim",
 	opts = {},
+	config = function()
+		require("mason").setup()
+
+		local ensure_installed = {
+		}
+
+		local registry = require("mason-registry")
+		registry.refresh(function()
+			for _, tool in ipairs(ensure_installed) do
+				local package = registry.get_package(tool)
+
+				if not package:is_installed() then
+					vim.notify(
+						string.format("Installing %s...", tool),
+						vim.log.levels.INFO
+					)
+					package:install()
+				end
+			end
+		end)
+	end,
 }
